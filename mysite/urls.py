253a375af742699ec4a13e5d8b33e669691ed872
Mike Covington <mfcovington@gmail.com>
2015-04-10 13:59:57 -0700

    Remove lab_members/ from project URLs

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,7 +12,6 @@ urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),  # NOQA
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
         {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^lab_members/', include('lab_members.urls', namespace='lab_members')),
     url(r'^', include('cms.urls')),
 )
 
