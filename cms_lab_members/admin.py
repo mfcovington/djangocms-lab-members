e9100bdc5d387dc62d513f94c1c5facb0f0a9b82
Mike Covington <mfcovington@gmail.com>
2015-04-10 12:44:10 -0700

    Override Scientist admin remove Bio fieldset

--- cms_lab_members/admin.py
+++ cms_lab_members/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from lab_members.models import Scientist
+from lab_members.admin import ScientistAdmin
+
+class CMSScientistAdmin(ScientistAdmin):
+    fieldsets = [
+        ScientistAdmin.fieldset_basic,
+        ScientistAdmin.fieldset_advanced,
+    ]
+
+admin.site.unregister(Scientist)
+admin.site.register(Scientist, CMSScientistAdmin)
