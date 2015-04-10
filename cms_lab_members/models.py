55765dfda5f2929bc054b611006f7adb605b81b4
Mike Covington <mfcovington@gmail.com>
2015-04-10 12:49:28 -0700

    Add Lab Members CMS plugin

--- cms_lab_members/models.py
+++ cms_lab_members/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cms.models import CMSPlugin
 
-# Create your models here.
+class ScientistPluginModel (CMSPlugin):
+    scientist = models.ForeignKey('lab_members.Scientist',
+        related_name='plugins'
+    )
+
+    def __str__(self):
+        return self.scientist.full_name
