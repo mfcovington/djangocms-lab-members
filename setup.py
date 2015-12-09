919ad1611e374c81a14e8b50fddde443f885d41e
Mike Covington <mfcovington@gmail.com>
2015-12-08 23:34:19 -0800

    Require django CMS >=3.0.7 and <3.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = [
     'Django>=1.7',
-    'django-cms>=3.0.7',
+    'django-cms>=3.0.7,<3.2',
     'django-lab-members>=0.3.0',
     'djangocms-lab-publications>=0.1.3',
 ]
